f34aba5f0b38d0458492948cf324852fed9cc622
Arturo Alcala <alcala28043@gmail.com>
2023-03-20 00:04:12 +0100

    add i18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,49 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpBackend, HttpClientModule } from '@angular/common/http';
+import { MultiTranslateHttpLoader } from 'ngx-translate-multi-http-loader';
+import { NgModule } from '@angular/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
+/**
+ * Description
+ *
+ * @export
+ * @param {HttpBackend} httpBackend
+ * @returns {*}
+ */
+export function HttpLoaderFactory(httpBackend: HttpBackend) {
+  return new MultiTranslateHttpLoader(httpBackend, [
+    { prefix: './assets/i18n/', suffix: '.json' },
+    //{ prefix: './assets/i18n/scrum-guide/', suffix: '.json' },
+  ]);
+}
+/**
+ * Description
+ *
+ * @export
+ * @class AppModule
+ * @typedef {AppModule}
+ */
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'es',
+      useDefaultLang: true,
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpBackend],
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
