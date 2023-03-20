0d890db1c78eeee0348de8ee6056d40c27c2d61e
Arturo Alcala <alcala28043@gmail.com>
2023-03-20 20:28:25 +0100

    menu-button component

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,13 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
 import { LoginButtonComponent } from './components/login-button/login-button.component';
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    LoginButtonComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LoginButtonComponent],
+  imports: [CommonModule, SharedModule],
+  exports: [LoginButtonComponent],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -4,9 +4,10 @@ import { HeaderComponent } from './components/header/header.component';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { TranslateModule } from '@ngx-translate/core';
+import { MenuButtonComponent } from './components/header/menu-button/menu-button.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, MenuButtonComponent],
   imports: [CommonModule, TranslateModule.forChild(), SharedModule, AuthModule],
   exports: [HeaderComponent],
 })
